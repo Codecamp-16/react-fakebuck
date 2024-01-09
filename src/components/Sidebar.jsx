6d9a50cb774edb12ee0d246a0b3d7d844a972e1e
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-09 14:44:53 +0700

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,18 @@
 import styled from '@emotion/styled';
 import React from 'react';
 import { Box } from '@mui/material';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import PeopleIcon from '@mui/icons-material/People';
 
 const Wrapper = styled(Box)(({ theme }) => ({
-  backgroundColor: 'red',
+  // backgroundColor: 'red',
   flex: 1,
   display: 'none',
   [theme.breakpoints.up('md')]: {
@@ -13,7 +22,42 @@ const Wrapper = styled(Box)(({ theme }) => ({
 }));
 
 function Sidebar() {
-  return <Wrapper>Sidebar</Wrapper>;
+  return (
+    <Wrapper>
+      <nav>
+        <List>
+          {/* Home */}
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary='Home Page' />
+            </ListItemButton>
+          </ListItem>
+          {/* Shop */}
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <StorefrontIcon />
+              </ListItemIcon>
+              <ListItemText primary='Shop' />
+            </ListItemButton>
+          </ListItem>
+
+          {/* Friend */}
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary='Friend' />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Wrapper>
+  );
 }
 
 export default Sidebar;
