4f10055e306c8f9a73d6756516d1c9fdf1d263cb
pavitpim40_mbp <pavitpim@gmail.com>
2024-01-09 14:49:27 +0700

    finish:side-bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,12 +8,15 @@ import { Box } from '@mui/material';
 // import ListItemIcon from '@mui/material/ListItemIcon';
 // import ListItemText from '@mui/material/ListItemText';
 import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import Switch from '@mui/material/Switch';
 import HomeIcon from '@mui/icons-material/Home';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import PeopleIcon from '@mui/icons-material/People';
+import ModeNightIcon from '@mui/icons-material/ModeNight';
 
 const Wrapper = styled(Box)(({ theme }) => ({
   // backgroundColor: 'red',
+  padding: '10px',
   flex: 1,
   display: 'none',
   [theme.breakpoints.up('md')]: {
@@ -27,7 +30,7 @@ function Sidebar() {
       <nav>
         <List>
           {/* Home */}
-          <ListItem disablePadding>
+          <ListItem>
             <ListItemButton>
               <ListItemIcon>
                 <HomeIcon />
@@ -36,7 +39,7 @@ function Sidebar() {
             </ListItemButton>
           </ListItem>
           {/* Shop */}
-          <ListItem disablePadding>
+          <ListItem>
             <ListItemButton>
               <ListItemIcon>
                 <StorefrontIcon />
@@ -46,12 +49,22 @@ function Sidebar() {
           </ListItem>
 
           {/* Friend */}
-          <ListItem disablePadding>
+          <ListItem>
             <ListItemButton>
               <ListItemIcon>
                 <PeopleIcon />
               </ListItemIcon>
               <ListItemText primary='Friend' />
+            </ListItemButton>
+          </ListItem>
+
+          {/* Mode Switch */}
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <ModeNightIcon />
+              </ListItemIcon>
+              <Switch />
             </ListItemButton>
           </ListItem>
         </List>
